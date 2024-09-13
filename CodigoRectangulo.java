7528d0704580086ed23e6ab80e49d48161c82e84
Adrian Rodriguez <adrianrv172023@gmail.com>
2024-09-12 21:34:59 -0600

    Codigo del rectangulo terminado

--- CodigoRectangulo.java
+++ CodigoRectangulo.java
@@ -1,3 +1,4 @@
+//Main
 public class Main {
     public static void main(String[] args) {
         Reactangulo rectangulo = new Reactangulo(2.15, 3.25);
@@ -6,5 +7,41 @@ public class Main {
 
         System.out.println("El area del rectangulo es: " + rectangulo.calcularArea());
         System.out.println("El perimetro del rectangulo es: " + rectangulo.calcularPerimetro());
+    }
+}
+
+//Rectangulo
+public class Reactangulo {
+    Double base;
+    Double altura;
+
+    public Reactangulo(Double base, Double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Double Base() {
+        return base;
+    }
+
+    public void Base(Double base) {
+        this.base = base;
+    }
+
+    public Double Altura() {
+        return altura;
+    }
+
+    public void Altura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double calcularArea() {
+        Double area = (base*altura);
+        return area;
+    }
+    public Double calcularPerimetro() {
+        Double perimetro = (base+altura)*2;
+        return perimetro;
     }
 }
